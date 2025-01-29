2a610ae2f2bb9f6ebc18ed4247311d08825b59cd
sumit <sk7627577@gmail.com>
2025-01-29 12:04:11 +0530

    update metadata and add Zoho SalesIQ script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,44 @@ import Whatsapp from "@/components/WhatsApp";
 import Call from "@/components/Call";
 import Footer2 from "@/components/Footer/Footer2";
 import LandingHeader from "@/components/Header/LandingHeader";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "",
-  description: "",
+  title: "Hotel and restaurant marketing company in usa | fielmente",
+  description:
+    "We're the best hospitality marketing agency in India serving hotels, resorts restaurants, and cloud kitchens for more than 10 years.",
+  keywords:"",
+  alternates: {
+    canonical: "https://https://dubai.fielmente.com/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  authors: [{ name: "Fielmente", url: "https://https://dubai.fielmente.com/" }],
+  openGraph: {
+    title: "Hotel and restaurant marketing company in usa | fielmente",
+    description:
+      "We're the best hospitality marketing agency in India serving hotels, resorts restaurants, and cloud kitchens for more than 10 years.",
+    url: "https://https://dubai.fielmente.com/",
+    siteName: "dubai.Fielmente",
+    locale: "en_IN",
+    type: "website",
+
+    images: [
+      {
+        url: "https://https://dubai.fielmente.com/images/Fielmente-og.png",
+        width: 1200,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -18,6 +52,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="zoho-init" strategy="afterInteractive">
+          {`
+          window.$zoho=window.$zoho || {};$zoho.salesiq=$zoho.salesiq||{ready:function(){}}
+          `}
+        </Script>
+        <Script
+          id="zsiqscript"
+          src="https://salesiq.zohopublic.in/widget?wc=siq1a53c0a073d244118c9a52feae425ec200f5a91c3949296330c5edf2c3a6e668"
+          strategy="afterInteractive"
+          defer
+        />
+      </head>
       <body suppressHydrationWarning={true}>
         <LandingHeader />
         {children}
